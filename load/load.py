7c104c8ac6200de334030dcd969d3b3bd4474875
David <120358712+davprog9@users.noreply.github.com>
2024-05-24 23:57:33 -0400

    Updated the load.py script, removed reduntant code by using the tempdir

--- load/load.py
+++ load/load.py
@@ -2,10 +2,10 @@ import os
 import gzip
 import shutil
 import duckdb
+import tempfile
 
 # Source and target directories
 source_directory = "data/gharchive_sample"
-target_dir = "data/unzipped"
 
 files = [file for file in os.listdir(source_directory) if file.endswith(".json.gz")]
 
@@ -20,19 +20,19 @@ def unzip_json_file(json_source, json_dest, filename):
             with open(target_file_path, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
-if not os.path.exists(target_dir):
+with tempfile.TemporaryDirectory(prefix="unzipped") as tempdir:
+    # Unzipping all the files 
     for file in files:
-        unzip_json_file(source_directory, target_dir, file)
+        unzip_json_file(source_directory, tempdir, file)
 
-con = duckdb.connect(database="github_stars.db")
+    # Creating database connection
+    con = duckdb.connect(database="github_stars.db")
 
-con.execute("CREATE SCHEMA IF NOT EXISTS source")
+    con.execute("CREATE SCHEMA IF NOT EXISTS source")
 
-json_files = [os.path.join(target_dir, f) for f in os.listdir(target_dir) if f.endswith('.json')]
-
-con.execute(f"""
+    con.execute(f"""
         CREATE OR REPLACE TABLE source.src_gharchive AS 
-        SELECT * FROM read_json_auto('{target_dir}/*.json')
+        SELECT * FROM read_json_auto('{tempdir}/*.json')
     """)
 
-con.close()
+    con.close()
