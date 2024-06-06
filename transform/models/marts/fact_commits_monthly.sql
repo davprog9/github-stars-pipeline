76ae20f63eab0cb6e4be787ba1e4d103e1f63cb5
David <120358712+davprog9@users.noreply.github.com>
2024-06-05 21:46:50 -0400

    Adding fact_commit and fact_stars monthly view's

---
+++ transform/models/marts/fact_commits_monthly.sql
@@ -0,0 +1,10 @@
+SELECT 
+    d.date_month as date_day,
+    s.repo_id,
+    COUNT(*) AS count_commits,
+    lag(count_commits, 12) OVER (PARTITION BY repo_id ORDER BY date_day) AS last_year_count_commit,
+    (count_commits / last_year_count_commit) - 1 AS yoy_growth
+FROM {{ ref('dim_date') }} AS d
+LEFT JOIN {{ ref('fact_commits') }} AS s
+ON d.date_month = DATE_TRUNC('month', s.event_date)
+GROUP BY date_day, repo_id
