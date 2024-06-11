838f229c21a00135b0e468bd5391bc14311f3b38
David <120358712+davprog9@users.noreply.github.com>
2024-06-11 01:58:12 -0400

    Added SCD for dim_repos.sql

--- transform/models/marts/dim_repos.sql
+++ transform/models/marts/dim_repos.sql
@@ -1,2 +1,7 @@
-SELECT DISTINCT repo_id, repo_name
-FROM {{ ref("stg_gharchive") }} 
+SELECT 
+repo_id, 
+repo_name, 
+min(event_date) AS start_date,
+lead(start_date) OVER (PARTITION BY repo_id ORDER BY start_date ASC) as date_end
+FROM {{ ref("stg_gharchive") }}
+GROUP BY 1, 2
